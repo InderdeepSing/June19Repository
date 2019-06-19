b5762ebfc7c558fb4c2127f1cb6bf1ee3efdafdf
Inderdeep Singh <753889@student.tor.loc>
2019-06-19 16:35:43 -0400

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello Class");
+// factory function
+function createCircle(radius){
+    return {
+       radius,
+       draw: function(){
+           console.log('draw');
+       }
+   };   
+   }
+   const circle = createCircle(1);
+   circle.draw();
+   
+   // Constructor Function
+   
+   function Circle(radius){
+       console.log('this', this);
+       this.radius = radius;
+       this.draw = function(){
+           console.log("draw");
+       }
+   }
+   
+   const another = new Circle(1);
+
+   const Circle1 = new Function('radius', `
+   this.radius = radius;
+   this.draw = function(){
+       console.log("draw");
+   }
+   `);
+   
+  
